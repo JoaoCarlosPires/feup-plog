6ee362ad13434880c3bd1d77a96b7d3929fcb45c
JoaoCarlosPires <joaocarlosmrp@gmail.com>
2020-11-23 20:24:42 +0000

    Topmost piece retired and changed for opponent piece

--- Trabalho/swack.pl
+++ Trabalho/swack.pl
@@ -61,27 +61,77 @@ playCycle :-
 	repeatCycle(0, 0).
 
 repeatCycle(NextPlayer, Pass) :-
+	board(A),
 	write('Move or Pass? (move/pass)'),nl,
 	read(Ans),nl,
 	(Ans == pass ->
 		(Pass == 1 -> 
-			winner(A)
+			winner
 		;
 			Next is NextPlayer + 1,
 			Player is mod(Next, 2),
 			display_game(Player),
 			repeatCycle(Player, 1) 
 		)
-	; 	% makeMove(A),
+	; 	makeMove(A, NextPlayer),
 		Next is NextPlayer + 1,
 		Player is mod(Next, 2),
 		display_game(Player), 
 		repeatCycle(Player, 0)
 	).
 
-% makeMove(board) :- 
+getInputPlay(Col, Lin) :-
+	getColumn(Col),
+	getLine(Lin).
 
-winner(board) :- write('Winner is ...').
+getLine(Lin) :-
+	write('Please specify line '),
+	get_code(Li),
+	skip_line,
+	validateLine(Li, Lin).
 
-initial(first) :- 
+getColumn(Col) :- 
+	write('Please specify column '), 
+	get_code(Co),
+	skip_line,
+	validateColumn(Co, Col).
+
+validateColumn(65, Col) :- Col is 0.
+validateColumn(66, Col) :- Col is 1.
+validateColumn(67, Col) :- Col is 2.
+validateColumn(68, Col) :- Col is 3.
+validateColumn(_, Col) :- write('Invalid column letter\n\n'), getColumn(Col).
+
+validateLine(48, Lin) :- Lin is 0.
+validateLine(49, Lin) :- Lin is 1.
+validateLine(50, Lin) :- Lin is 2.
+validateLine(51, Lin) :- Lin is 3.
+validateLine(_, Lin) :- write('Invalid line number\n\n'), getLine(Lin).
+
+makeMove(Board, Next) :-
+	getInputPlay(Col, Lin),
+	nth0(Lin, Board, BoardLine),
+	nth0(Col, BoardLine, BoardCol),
+	nth0(0, BoardCol, P),
+	NextPlayer is Next + 1,
+	(P \= NextPlayer
+		-> write('Cannot move an adversary piece!\n'),
+		makeMove(Board, Next)
+		; 
+		(P == 1 -> NewP is 2; NewP is 1),
+		myreplace(BoardCol, New, NewP),
+		replace(Col, BoardLine, New, NewLine),
+		replace(Lin, Board, NewLine, NewBoard),
+		write(NewBoard), nl
+	).
+
+myreplace([Head|Tail], New, Piece) :- append([], [Piece|Tail], New). 
+
+replace(I, L, E, K) :-
+  nth0(I, L, _, R),
+  nth0(I, K, E, R).
+
+winner :- write('Winner is ...').
+
+initial(first) :-
 	display_game(0).
